7de78e03bd2595db6a283907a300d823f60f9937
Dan Carlier <dan.carlier@gmail.com>
2023-02-24 13:49:29 +1100

    Allow toggling between connection uri's

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
 const { MongoClient } = require('mongodb')
 
 let dbConnection
+let uri = 'mongodb://localhost:27017/bookstore' // local
+//let uri = 'mongodb+srv://<username>:<password>@cluster0.ew3syjk.mongodb.net/?retryWrites=true&w=majority'
 
 module.exports = {
   connectToDb: (cb) => {
-    MongoClient.connect('mongodb://localhost:27017/bookstore')
+    MongoClient.connect(uri)
       .then((client) => {
         dbConnection = client.db()
         return cb()
